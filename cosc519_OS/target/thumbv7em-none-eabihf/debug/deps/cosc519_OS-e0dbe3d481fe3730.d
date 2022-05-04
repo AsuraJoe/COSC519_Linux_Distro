b98a8a8a9be2e6a395984a3b1e12f26b9c833806
AsuraJoe <skyrider245@gmail.com>
2022-05-04 18:38:14 -0400

    remove unecessary folder

--- cosc519_OS/target/thumbv7em-none-eabihf/debug/deps/cosc519_OS-e0dbe3d481fe3730.d
+++
@@ -1,5 +0,0 @@
-D:\COSC519_Linux_Distro\cosc519_OS\target\thumbv7em-none-eabihf\debug\deps\cosc519_OS-e0dbe3d481fe3730: src\main.rs
-
-D:\COSC519_Linux_Distro\cosc519_OS\target\thumbv7em-none-eabihf\debug\deps\cosc519_OS-e0dbe3d481fe3730.d: src\main.rs
-
-src\main.rs:
